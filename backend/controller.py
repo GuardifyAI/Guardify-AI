3b3de74f4bb762b793fe1f3cda6ee85e687f0395
yonatan.r@razor-labs.com <yonatan.r@razor-labs.com>
2025-08-02 18:50:27 +0300

    started controller and added a health request

---
+++ backend/controller.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from backend.logic.app_logic import AppLogic
+
+RESULT_KEY = "result"
+ERROR_MESSAGE_KEY = "errorMessage"
+
+class Controller:
+    def __init__(self, app: Flask):
+        self.app = app
+        self.book_logic = AppLogic()
+        self.setup_routes()  # Ensure routes are set up during initialization
+
+    def setup_routes(self):
+        @self.app.route("/app/health", methods=["GET"])
+        def get_health():
+            return "OK", 200
