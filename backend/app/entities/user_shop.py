66bf9e98ff9ebb168d4a52af577f9e04fe4d0665
yonatan.r@razor-labs.com <yonatan.r@razor-labs.com>
2025-08-05 20:08:22 +0300

    added user_shop entity

---
+++ backend/app/entities/user_shop.py
@@ -0,0 +1,28 @@
+from backend.app import db
+from dataclasses import dataclass
+
+
+@dataclass
+class UserShopDTO:
+    user_id: str
+    shop_id: str
+
+
+class UserShop(db.Model):
+    __tablename__ = 'user_shop'
+
+    user_id = db.Column(db.Text, db.ForeignKey('user.user_id'), primary_key=True)
+    shop_id = db.Column(db.Text, db.ForeignKey('shop.shop_id'), primary_key=True)
+
+    # Relationships
+    user = db.relationship('User', backref='user_shops')
+    shop = db.relationship('Shop', backref='user_shops')
+
+    def __repr__(self):
+        return f"<UserShop user_id={self.user_id} shop_id={self.shop_id}>"
+
+    def to_dto(self) -> UserShopDTO:
+        return UserShopDTO(
+            user_id=self.user_id,
+            shop_id=self.shop_id,
+        )
